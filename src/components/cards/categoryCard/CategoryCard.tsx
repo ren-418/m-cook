ea31b02133798de17ab1769b40491ca52161fa5a
ren-418 <mrsteevtvw418@gmail.com>
2024-10-31 10:50:10 -0300

    reels

--- src/components/cards/categoryCard/CategoryCard.tsx
+++ src/components/cards/categoryCard/CategoryCard.tsx
@@ -9,7 +9,7 @@ const CategoryCard: React.FC<categoryCardProps> = ({ imageUrl, categoryName, onC
     return (
         <div className="flex items-center justify-start w-[156px] h-[60px] p-[8px] shadow-[0_0_4px_1px_rgba(0,0,0,0.1),0_0_1px_0_rgba(0,0,0,0.5)] rounded-[8px] ">
             <img src={imageUrl} alt={categoryName} className="w-[40px] h-[40px] object-cover rounded-[8px] "/>
-            <p className="flex text-[14px] w-[100%] h-[100%] px-[8px] items-center text-body-b text-neutral-800 overflow-hidden text-ellipsis"
+            <p className="flex text-[14px] w-[100%] h-[90%] px-[8px] items-center text-body-b text-neutral-800 overflow-hidden text-ellipsis"
                style={{
                    display: '-webkit-box',
                    WebkitBoxOrient: 'vertical',
