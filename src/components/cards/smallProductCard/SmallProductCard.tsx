1ea0e2cd1aaefbd39d91ed5ef862d449207c52b3
ren-418 <mrsteevtvw418@gmail.com>
2024-11-06 20:51:50 -0300

    Fixed carrousel and small cards

--- src/components/cards/smallProductCard/SmallProductCard.tsx
+++ src/components/cards/smallProductCard/SmallProductCard.tsx
@@ -2,14 +2,18 @@
 interface smallProductCardProps {
   imageUrl: string;
   productName: string;
-  onClick: () => void
+  onClick?: () => void
 }
 
-const SmallProductCard: React.FC<smallProductCardProps> = ({ imageUrl, productName, onClick }) => {
+const SmallProductCard: React.FC<smallProductCardProps> = ({ imageUrl, productName, onClick = () => {} }) => {
     return (
         <button onClick={onClick} className="flex items-center justify-start w-[168px] h-[56px] p-[8px] shadow-[0_0_4px_1px_rgba(0,0,0,0.1),0_0_1px_0_rgba(0,0,0,0.5)] rounded-[8px] ">
-            <img src={imageUrl} alt={productName} className="w-[40px] h-[40px] object-cover rounded-[8px] "/>
-            <p className="flex text-[14px] w-[100%] h-[100%] px-[8px] items-center text-body-b text-neutral-800 overflow-hidden text-ellipsis"
+            <img 
+            src={imageUrl} 
+            alt={productName} 
+            className="w-[40px] h-[40px] object-cover rounded-[8px] "
+            draggable="false"/>
+            <p className="flex-1 flex items-center text-left select-none px-[8px] text-body-b text-neutral-800 overflow-hidden text-ellipsis"
                style={{
                    display: '-webkit-box',
                    WebkitBoxOrient: 'vertical',
--- src/components/common/carrousel/Carrousel.tsx
+++ src/components/common/carrousel/Carrousel.tsx
@@ -1,38 +1,25 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef } from "react";
 
 interface CarrouselProps {
     children: React.ReactNode;
     title: string;
-    width: string;
 }
 
-const Carrousel: React.FC<CarrouselProps> = ({ children, title, width = "100%" }) => {
+const Carrousel: React.FC<CarrouselProps> = ({ children, title }) => {
     const carrouselRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        const carrousel = carrouselRef.current;
-        if (carrousel) {
-            const handleScroll = () => {
-                if (carrousel.scrollLeft === 0) {
-                    carrousel.scrollLeft = carrousel.scrollWidth / 2;
-                } else if (carrousel.scrollLeft >= carrousel.scrollWidth - carrousel.clientWidth) {
-                    carrousel.scrollLeft = carrousel.scrollWidth / 2 - carrousel.clientWidth;
-                }
-            };
-            carrousel.addEventListener("scroll", handleScroll);
-            carrousel.scrollLeft = carrousel.scrollWidth / 2;
-            return () => carrousel.removeEventListener("scroll", handleScroll);
-        }
-    }, []);
-
-    const clonedChildren = React.Children.toArray(children).concat(React.Children.toArray(children));
-
     return (
-        <div className="flex flex-col" style={{ width }}>
-            <h2 className="text-[24px] text-neutral-black text-h4 mb-2.5 ml-2.5">{title}</h2>
-            <div ref={carrouselRef} className="flex w-full overflow-x-auto scrollbar-hide space-x-4 px-[4px] snap-x snap-mandatory py-[2px] h-auto items-center justify-start">
-                {clonedChildren.map((child, index) => (
-                    <div key={index} className="snap-start">{child}</div>
+        <div className="flex flex-col w-full">
+            <h4 className="text-neutral-800 mb-[10px] ml-[16px]">{title}</h4>
+            <div 
+                ref={carrouselRef} 
+                className="flex w-full scrollbar-hide overflow-scroll space-x-4 pl-[16px] snap-x snap-mandatory py-[2px] h-auto items-center justify-start" 
+                style={{
+                    clipPath: "inset(0 -10px 0 0)"
+                }}
+            >
+                {React.Children.map(children, (child, index) => (
+                    <div key={index}>{child}</div>
                 ))}
             </div>
         </div>
