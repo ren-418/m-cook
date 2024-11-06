f336276d875cf3b1fe02c8f6f2963743c4147f69
ren-418 <mrsteevtvw418@gmail.com>
2024-11-06 10:02:04 -0300

    Grid, category card, small product card and carrousel

--- src/components/cards/smallProductCard/SmallProductCard.tsx
+++ src/components/cards/smallProductCard/SmallProductCard.tsx
@@ -7,7 +7,7 @@ interface smallProductCardProps {
 
 const SmallProductCard: React.FC<smallProductCardProps> = ({ imageUrl, productName, onClick }) => {
     return (
-        <div className="flex items-center justify-start w-[168px] h-[56px] p-[8px] shadow-[0_0_4px_1px_rgba(0,0,0,0.1),0_0_1px_0_rgba(0,0,0,0.5)] rounded-[8px] ">
+        <button onClick={onClick} className="flex items-center justify-start w-[168px] h-[56px] p-[8px] shadow-[0_0_4px_1px_rgba(0,0,0,0.1),0_0_1px_0_rgba(0,0,0,0.5)] rounded-[8px] ">
             <img src={imageUrl} alt={productName} className="w-[40px] h-[40px] object-cover rounded-[8px] "/>
             <p className="flex text-[14px] w-[100%] h-[100%] px-[8px] items-center text-body-b text-neutral-800 overflow-hidden text-ellipsis"
                style={{
@@ -16,7 +16,7 @@ const SmallProductCard: React.FC<smallProductCardProps> = ({ imageUrl, productNa
                    WebkitLineClamp: 2,
                }}
             >{productName}</p>
-        </div>
+        </button>
     );
 };
 
