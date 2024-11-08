f3d4cf465e8a4166c10751f71801e747f9092b4b
ren-418 <mrsteevtvw418@gmail.com>
2024-11-08 17:11:35 -0300

    Like button card

--- src/components/cards/productCard/ProductCard.tsx
+++ src/components/cards/productCard/ProductCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PriceBox from '../../common/priceBox/PriceBox'
 import Button from '../../common/button/Button'
+import LikeButton from '../../common/likeButton/LikeButton'
 
 interface ProductCardProps {
   ingredientName: string,
@@ -15,6 +16,9 @@ interface ProductCardProps {
 function ProductCards({ingredientName, brandName, activeIcons, imageSrc, price, discount=0, onClick=()=>{}}: ProductCardProps) {
   return (
     <div className='select-none flex flex-col overflow-hidden rounded-[12px] shadow-card bg-neutral-white w-[170px] h-[252px]'>
+      <div className='absolute top-[13px] right-[13px] z-10'>
+        <LikeButton />
+      </div>
       <img draggable={false} src={imageSrc} className='w-full h-[93px] object-cover'/>
       <div className='flex-1 w-full p-[8px] flex flex-col justify-between items-left'>
         <p className='text-body-b text-neutral-800 w-full overflow-hidden text-ellipsis'
--- src/components/common/likeButton/LikeButton.tsx
+++ src/components/common/likeButton/LikeButton.tsx
@@ -3,11 +3,11 @@ import HeartIcon from "../../../icons/likeButton/HeartIcon";
 import HeartFilledIcon from "../../../icons/likeButton/HeartFilledIcon";
 
 interface LikeButtonProps {
-    onLike: () => void;
-    onNotLike: () => void;
+    onLike?: () => void;
+    onNotLike?: () => void;
 }
 
-const LikeButton: React.FC<LikeButtonProps> = ({ onLike = () => {}, onNotLike = () => {} }) => {
+const LikeButton: React.FC<LikeButtonProps> = ({ onLike = () => {}, onNotLike = () => {}}) => {
     const [liked, setLiked] = React.useState(false);
 
     const handleClick = () => {
@@ -22,7 +22,7 @@ const LikeButton: React.FC<LikeButtonProps> = ({ onLike = () => {}, onNotLike = 
     return (
         <button
             onClick={handleClick}
-            className="shadow-[0_0_4px_1px_rgba(0,0,0,0.1),0_0_1px_0_rgba(0,0,0,0.5)] w-[34px] h-[34px] rounded-[50%] bg-white flex items-center justify-center"
+            className="shadow-card w-[34px] h-[34px] rounded-[50%] bg-neutral-white flex items-center justify-center"
         >
             {liked ? <HeartFilledIcon/> : <HeartIcon/>}
         </button>
