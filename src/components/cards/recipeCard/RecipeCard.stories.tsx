d9f9d1647d8d5d51a63ad25c77d4f9ed0e01978d
ren-418 <mrsteevtvw418@gmail.com>
2024-11-09 20:00:42 -0300

    recipe card

---
+++ src/components/cards/recipeCard/RecipeCard.stories.tsx
@@ -0,0 +1,41 @@
+import RecipeCard from "./RecipeCard";
+import {Meta, StoryObj} from "@storybook/react";
+
+const meta = {
+    title: 'Cards/RecipeCard',
+    component: RecipeCard,
+
+    parameters: {
+        layout: 'centered',
+    },
+
+    tags: ['autodocs'],
+
+    argTypes: {
+        ingredientName: {
+            control: 'text',
+        },
+        brandName: {
+            control: 'text',
+        },
+        activeIcons: {
+            control: 'object',
+        },
+        imageSrc: {
+            control: 'text',
+        },
+    },
+} satisfies Meta<typeof RecipeCard>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const RecipeCardDefault: Story = {
+    args: {
+        ingredientName: 'Recipe Name Could Be Max Two Lines Large',
+        brandName: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore',
+        activeIcons: [true, false, true, false],
+        imageSrc: 'https://th.bing.com/th/id/OIP.74L1tQJeO5RJeCqyLFyWPAHaFb?w=246&h=181&c=7&r=0&o=5&dpr=1.3&pid=1.7',
+    },
+};
